8291f88dc2df1a4c76159432ca3bd61ff34d244d
Narendra0086 <indranar.babu@gmail.com>
2024-08-30 18:03:39 +0530

    Update main.tf

--- resource/main.tf
+++ resource/main.tf
@@ -1,1 +1,326 @@
+resource "azurerm_resource_group" "aks" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
 
+resource "azurerm_user_assigned_identity" "msi" {
+  location            = var.location
+  name                = format("%s-msi", var.name)
+  resource_group_name = azurerm_resource_group.aks.name
+  tags                = var.tags
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                              = "${var.name}-aks"
+  location                          = azurerm_resource_group.aks.location
+  resource_group_name               = azurerm_resource_group.aks.name
+  dns_prefix                        = var.name
+  kubernetes_version                = var.kubernetes_version
+  node_resource_group               = var.node_resource_group
+  azure_policy_enabled              = var.azure_policy_enabled
+  node_os_channel_upgrade           = var.node_os_channel_upgrade
+  automatic_channel_upgrade         = var.automatic_channel_upgrade
+  role_based_access_control_enabled = true
+  workload_identity_enabled         = var.workload_identity_enabled
+  oidc_issuer_enabled               = var.oidc_issuer_enabled
+  tags                              = var.tags
+
+  dynamic "maintenance_window_node_os" {
+    for_each = var.maintenance_window_node_os != null ? [1] : []
+    content {
+      frequency   = var.maintenance_window_node_os.frequency
+      interval    = var.maintenance_window_node_os.interval
+      duration    = var.maintenance_window_node_os.duration
+      day_of_week = var.maintenance_window_node_os.day_of_week
+      start_time  = var.maintenance_window_node_os.start_time
+    }
+  }
+
+  dynamic "default_node_pool" {
+    for_each = { for k, v in local.agent_pools : k => v if k == local.default_pool }
+    iterator = ap
+    content {
+      name                        = ap.value.name
+      node_count                  = ap.value.count
+      vm_size                     = ap.value.vm_size
+      zones                       = ap.value.availability_zones
+      enable_auto_scaling         = ap.value.enable_auto_scaling
+      min_count                   = ap.value.min_count
+      max_count                   = ap.value.max_count
+      max_pods                    = ap.value.max_pods
+      os_disk_size_gb             = ap.value.os_disk_size_gb
+      type                        = ap.value.type
+      vnet_subnet_id              = ap.value.vnet_subnet_id
+      node_taints                 = ap.value.node_taints
+      orchestrator_version        = ap.value.orchestrator_version
+      temporary_name_for_rotation = ap.value.temporary_name_for_rotation
+    }
+  }
+
+  dynamic "key_vault_secrets_provider" {
+    for_each = var.key_vault_secrets_provider.enabled ? [true] : []
+    content {
+      secret_rotation_enabled  = var.key_vault_secrets_provider.secret_rotation_enabled
+      secret_rotation_interval = var.key_vault_secrets_provider.secret_rotation_interval
+    }
+  }
+
+  dynamic "oms_agent" {
+    for_each = var.oms_agent_log_analytics_workspace_id != null ? [true] : []
+    content {
+      log_analytics_workspace_id = var.oms_agent_log_analytics_workspace_id
+    }
+  }
+
+  dynamic "linux_profile" {
+    for_each = var.linux_profile != null ? [true] : []
+    content {
+      admin_username = var.linux_profile.username
+
+      ssh_key {
+        key_data = var.linux_profile.ssh_key
+      }
+    }
+  }
+
+  dynamic "windows_profile" {
+    for_each = var.windows_profile != null ? [true] : []
+    content {
+      admin_username = var.windows_profile.username
+      admin_password = var.windows_profile.password
+    }
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+    dns_service_ip = cidrhost(var.service_cidr, 10)
+    service_cidr   = var.service_cidr
+
+    # Use Standard if availability zones are set, Basic otherwise
+    load_balancer_sku = local.load_balancer_sku
+  }
+
+  azure_active_directory_role_based_access_control {
+    managed                = true
+    admin_group_object_ids = var.cluster_admins
+    azure_rbac_enabled     = var.azure_rbac_enabled
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.msi.id]
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "aks" {
+  for_each = { for k, v in local.agent_pools : k => v if k != local.default_pool }
+
+  name                  = each.key
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = each.value.vm_size
+  zones                 = each.value.availability_zones
+  enable_auto_scaling   = each.value.enable_auto_scaling
+  node_count            = each.value.count
+  min_count             = each.value.min_count
+  max_count             = each.value.max_count
+  max_pods              = each.value.max_pods
+  os_disk_size_gb       = each.value.os_disk_size_gb
+  os_type               = each.value.os_type
+  vnet_subnet_id        = each.value.vnet_subnet_id
+  node_taints           = each.value.node_taints
+  orchestrator_version  = each.value.orchestrator_version
+
+  tags = var.tags
+}
+
+data "azurerm_monitor_diagnostic_categories" "default" {
+  resource_id = azurerm_kubernetes_cluster.aks.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "aks" {
+  count = var.diagnostics != null ? 1 : 0
+
+  name                           = "${var.name}-aks-diag"
+  target_resource_id             = azurerm_kubernetes_cluster.aks.id
+  log_analytics_workspace_id     = local.parsed_diag.log_analytics_id
+  eventhub_authorization_rule_id = local.parsed_diag.event_hub_auth_id
+  eventhub_name                  = local.parsed_diag.event_hub_auth_id != null ? var.diagnostics.eventhub_name : null
+  storage_account_id             = local.parsed_diag.storage_account_id
+
+  dynamic "enabled_log" {
+    for_each = {
+      for k, v in data.azurerm_monitor_diagnostic_categories.default.log_category_types : k => v
+      if contains(local.parsed_diag.log, "all") || contains(local.parsed_diag.log, v)
+    }
+    content {
+      category = enabled_log.value
+    }
+  }
+
+  dynamic "metric" {
+    for_each = data.azurerm_monitor_diagnostic_categories.default.metrics
+    content {
+      category = metric.value
+      enabled  = contains(local.parsed_diag.metric, "all") || contains(local.parsed_diag.metric, metric.value)
+    }
+  }
+}
+
+# Assign roles
+
+# https://learn.microsoft.com/en-us/azure/aks/manage-azure-rbac
+resource "azurerm_role_assignment" "users" {
+  for_each = { for u in var.cluster_users : format("%s-%s", u.principal_id, u.namespace) => u }
+
+  principal_id         = each.value.principal_id
+  scope                = format("%s/namespaces/%s", azurerm_kubernetes_cluster.aks.id, each.value.namespace)
+  role_definition_name = "Azure Kubernetes Service RBAC Writer"
+}
+
+resource "azurerm_role_assignment" "acr" {
+  count = length(var.container_registries)
+
+  scope                = var.container_registries[count.index]
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.msi.principal_id
+}
+
+resource "azurerm_role_assignment" "subnet" {
+  count = length(local.agent_pool_subnets)
+
+  scope                = local.agent_pool_subnets[count.index]
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_user_assigned_identity.msi.principal_id
+}
+
+resource "azurerm_role_assignment" "storage" {
+  count = length(var.storage_contributor)
+
+  scope                = var.storage_contributor[count.index]
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_user_assigned_identity.msi.principal_id
+}
+
+resource "azurerm_role_assignment" "msi" {
+  count = length(var.managed_identities)
+
+  scope                = var.managed_identities[count.index]
+  role_definition_name = "Managed Identity Operator"
+  principal_id         = azurerm_user_assigned_identity.msi.principal_id
+}
+
+# Configure cluster
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
+}
+
+#
+# Impersonation of admins
+#
+
+resource "kubernetes_cluster_role" "impersonator" {
+  metadata {
+    name = "impersonator"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["users", "groups", "serviceaccounts"]
+    verbs      = ["impersonate"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "impersonator" {
+  count = length(var.admins)
+
+  metadata {
+    name = "${var.admins[count.index].name}-administrator"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.impersonator.metadata.0.name
+  }
+
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = var.admins[count.index].kind
+    name      = var.admins[count.index].name
+  }
+}
+
+#
+# Container logs for Azure
+#
+
+resource "kubernetes_cluster_role" "containerlogs" {
+  metadata {
+    name = "containerhealth-log-reader"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods/log"]
+    verbs      = ["get"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "containerlogs" {
+  metadata {
+    name = "containerhealth-read-logs-global"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.containerlogs.metadata.0.name
+  }
+
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "User"
+    name      = "clusterUser"
+  }
+}
+
+#
+# Service accounts
+#
+
+resource "kubernetes_service_account" "sa" {
+  count = length(var.service_accounts)
+
+  metadata {
+    name      = var.service_accounts[count.index].name
+    namespace = var.service_accounts[count.index].namespace
+  }
+
+  automount_service_account_token = true
+}
+
+resource "kubernetes_cluster_role_binding" "sa" {
+  count = length(var.service_accounts)
+
+  metadata {
+    name = var.service_accounts[count.index].name
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = var.service_accounts[count.index].role
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = var.service_accounts[count.index].name
+    namespace = var.service_accounts[count.index].namespace
+  }
+}

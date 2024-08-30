d7ac391a92f1698d2636b43973c943a0493ab029
Narendra0086 <indranar.babu@gmail.com>
2024-08-30 18:11:20 +0530

    Update provider.tf

--- resource/provider.tf
+++ resource/provider.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.98.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}

adcf9138a454fb5e569173838ca1d8da0a492510
Narendra0086 <indranar.babu@gmail.com>
2024-08-30 18:04:00 +0530

    Update output.tf

--- resource/output.tf
+++ resource/output.tf
@@ -1,1 +1,15 @@
+output "id" {
+  description = "The Kubernetes Managed Cluster ID."
+  value       = azurerm_kubernetes_cluster.aks.id
+}
 
+output "host" {
+  description = "The Kubernetes cluster server host."
+  value       = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  sensitive   = true
+}
+
+output "identity" {
+  description = "The AKs managed identity Object(principal) ID."
+  value       = azurerm_user_assigned_identity.msi.principal_id
+}

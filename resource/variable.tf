b5523f736da9ff4df13e00a4ff27f3c57a989a83
Narendra0086 <indranar.babu@gmail.com>
2024-08-30 18:04:17 +0530

    Update variable.tf

--- resource/variable.tf
+++ resource/variable.tf
@@ -1,1 +1,11 @@
+variable "name" {
+  description = "Name of the Kubernetes cluster."
+}
 
+variable "resource_group_name" {
+  description = "Name of resource group to deploy resources in."
+}
+
+variable "location" {
+  description = "The Azure Region in which to create resource."
+}
